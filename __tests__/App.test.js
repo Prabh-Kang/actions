77694dd3a09548d773b417a3dc4a4416c37e577d
Prabh-Kang <prbdpkng@gmail.com>
2022-10-05 19:18:33 +0530

    [ADDED]: selenium tests

---
+++ __tests__/App.test.js
@@ -0,0 +1,24 @@
+const { By } = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
+
+describe("App Component", () => {
+  let driver;
+  beforeAll(async() => {
+    driver = await new webdriver.Builder().forBrowser(webdriver.Browser.CHROME).build();
+    await driver.navigate().to("http://localhost:5173");
+  })
+
+  afterAll(async() => {
+    await driver.close();
+  })
+
+  it("should fetch the posts", async() => {
+    await driver.wait(async() => {
+      const posts = await driver.findElements(By.css("li"));
+      return posts.length !==0;
+    })
+    const posts = await driver.findElements(By.css("li"));
+    expect(posts.length).toBe(1)
+    expect(await posts[0].getText()).toBe("json-server");
+  })
+})
