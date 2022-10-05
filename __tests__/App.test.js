d7e184f14f7abddc8635b3b8b44ed2d8ad446ed1
Prabh-Kang <prbdpkng@gmail.com>
2022-10-05 20:13:16 +0530

    changes

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,10 +1,13 @@
 const { By } = require('selenium-webdriver');
 const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome')
 
 describe("App Component", () => {
   let driver;
   beforeAll(async() => {
-    driver = await new webdriver.Builder().forBrowser(webdriver.Browser.CHROME).build();
+    const options = new chrome.Options();
+    options.addArguments('headless');
+    driver = await new webdriver.Builder().forBrowser(webdriver.Browser.CHROME).setChromeOptions(options).build();
     await driver.navigate().to("http://localhost:5173");
   })
 
